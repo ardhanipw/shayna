65fdb4b7200f262f75fa942d11e37e9c4ff8a97b
ardhanipw <ardhanipw@student.undip.ac.id>
2020-09-18 19:48:34 +0700

    commit pertama

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    
+    <HeaderShayna />
+    <HeroShayna />
+    <ProductSliderShayna />
+    <SocialMediaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import ProductSliderShayna from '@/components/ProductSliderShayna.vue'
+import SocialMediaShayna from '@/components/SocialMediaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    ProductSliderShayna,
+    SocialMediaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
